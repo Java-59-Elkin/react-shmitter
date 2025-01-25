de59f69dad49b045eac864e474519995fe7db0b1
Mikhail <elkinms85@gmail.com>
2025-01-25 23:31:49 +0200

    Add function changeAvatar and PreventDefault to Avatar module

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,18 +2,18 @@ import {useContext} from "react";
 import {TwitterContext} from "../utils/context.js";
 
 const Avatar = ({size}) => {
-    const {user, changeAvatar} = useContext(TwitterContext);
+    const {user, changeAvatar, changeName} = useContext(TwitterContext);
 
     return (
         <img
             onClick={() => {
                 const url = prompt('Enter new avatar url');
-                changeAvatar.log(url);
+                changeAvatar(url);
             }}
-            onContextMenu={() => {
+            onContextMenu={(e) => {
                 e.preventDefault();
                 const name = prompt('Enter new name');
-                chngeNAme(name);
+                changeName(name);
             }}
             className={`user-avatar ${size ?? ''}`}
             src={user.avatar}
